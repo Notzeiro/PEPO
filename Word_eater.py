6b370de1f33d5a1ce894bdf74a83c07c7262e3d8
notzeiro <notzeiro@gmail.com>
2025-04-22 16:43:06 -0400

    hola

---
+++ Word_eater.py
@@ -0,0 +1,8 @@
+user_word=str(input("Enter a word "))
+user_word = user_word.upper()
+for letter in user_word:
+  if letter in {"A" , "E" , "I" , "O" , "U" }:
+    continue
+  else: 
+       print(letter)
+    
